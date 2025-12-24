d94ffe34039950dd151e32151cbf4dbab1f81ced
Tanuj Sharma <ta904513@gmail.com>
2025-12-24 16:45:52 +0530

    [Tanuj Sharma] Add . constructors problems solved

--- oops-pratctice/gcr-codebase/constructors/Books.java
+++ oops-pratctice/gcr-codebase/constructors/Books.java
@@ -1,18 +1,22 @@
+
 public class Books {
-	String title ;
-	String author;
-	int prize ;
-	
-	 Books(String title ,String author ,int prize)
-	{
-		this.title=title;
-		this.author =author;
-		this.prize=prize;
-	}
-	 Books() {}
-	public static void main(String[] args) {
-		
-		Books book = new Books("piyashi javani ","Tanuj sharma",150);
-	}
+
+    String title;
+    String author;
+    int prize;
+
+    Books(String title, String author, int prize) {
+        this.title = title;
+        this.author = author;
+        this.prize = prize;
+    }
+
+    Books() {
+    }
+
+    public static void main(String[] args) {
+
+        Books book = new Books("meri javani  ", "Tanuj sharma", 150);
+    }
 
 }
