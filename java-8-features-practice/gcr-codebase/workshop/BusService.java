20297384eb0ea329264ab0100ef6e55534af5921
Tanuj Sharma <ta904513@gmail.com>
2026-02-02 13:01:26 +0530

    [Tanuj sharma ] add . workshop question

---
+++ java-8-features-practice/gcr-codebase/workshop/BusService.java
@@ -0,0 +1,5 @@
+package workshop;
+
+public class BusService implements TransportService {
+
+}
---
+++ java-8-features-practice/gcr-codebase/workshop/UserInterface.java
@@ -0,0 +1,10 @@
+package workshop;
+
+public class UserInterface {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
